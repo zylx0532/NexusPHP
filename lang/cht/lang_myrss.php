1b560406e6f53a02706f87b7cd9e481afb549c77
cdwolfling <cdwolfling@gmail.com>
2017-12-25 16:52:22 +0800

    remove config/ucenter.php; add lang\chs\lang_attendance.php,lang_complains.php,lang_myrss.php; add lang\cht\lang_attendance.php,lang_complains.php,lang_myrss.php,donate.php; add lang\en\lang_attendance.php,lang_complains.php,lang_myrss.php; add attendance.php, myrss.php;

---
+++ lang/cht/lang_myrss.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang_myrss = array
+(
+	'text_myrss_down' => '的下載筐',
+	'text_rss_address' => '訂閱地址：',
+	'text_how_to' => '如何使用？ ',
+	'std_nothing_found' => "沒有結果！",
+	'std_no_active_torrents' => "沒有種子:( \n",
+	'head_message' => "提示信息",
+	'msg_torrent_added' => "指定的種子已經添加到列表中",
+	'msg_torrent_deleted' => "指定的種子已經從列表中刪除",
+	'is_already_added' => '已经添加过了。'
+);
+
+?>
